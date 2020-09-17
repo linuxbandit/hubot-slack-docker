1388d6f8385459ca293fab3dcd27854cd7b31a6d
linuxbandit <bellicaf@tcd.ie>
2020-09-17 22:12:53 +0300

    feat: add version query

--- scripts/myaegeestatus.js
+++ scripts/myaegeestatus.js
@@ -2,21 +2,28 @@
 //   Query services status - simple reading of my.aegee.eu/status
 //
 //   Examples:
-//   - `hubot what is the status of frontend` - Get status of frontend
-//   - `hubot what is the status of core` - Get status of core
-//   - `hubot what is the status of events` - Get status of events
-//   - `hubot what is the status of statutory` - Get status of statutory
-//   - `hubot what is the status of discounts` - Get status of discounts
-//   - `hubot what is the status of mailer` - Get status of mailer
+//   - `hubot what status is frontend` - Get status of frontend
+//   - `hubot what version is frontend` - Get version of frontend
+//   - `hubot what status is core` - Get status of core
+//   - `hubot what version is core` - Get version of core
+//   - `hubot what status is events` - Get status of events
+//   - `hubot what version is events` - Get version of events
+//   - `hubot what status is statutory` - Get status of statutory
+//   - `hubot what version is statutory` - Get version of statutory
+//   - `hubot what status is discounts` - Get status of discounts
+//   - `hubot what version is discounts` - Get version of discounts
+//   - `hubot what status is mailer` - Get status of mailer
+//   - `hubot what version is mailer` - Get version of mailer
 //
 // Commands:
-//   hubot what is the status of (frontend|core|events|statutory|discounts|mailer)
+//   hubot what status is (frontend|core|events|statutory|discounts|mailer)
+//   hubot what version is (frontend|core|events|statutory|discounts|mailer)
 //
 // Author:
 //   https://github.com/linuxbandit
 
 module.exports = (robot) => {
-  robot.hear(/what is the status of (frontend|core|events|statutory|discounts|mailer)/i, (res) => {
+  robot.hear(/what status is (frontend|core|events|statutory|discounts|mailer)/i, (res) => {
       
       const service = res.match[1]
       const base_url = "https://my.aegee.eu"
@@ -43,4 +50,34 @@ module.exports = (robot) => {
       })
   })
 
+  robot.hear(/what version is (frontend|core|events|statutory|discounts|mailer)/i, (res) => {
+      
+    const service = res.match[1]
+    const base_url = "https://my.aegee.eu"
+    const path = (service === "frontend" ? "/healthcheck" : `/api/${service}/healthcheck`)
+
+    robot.http(base_url + path)
+      .header('Accept', 'application/json')
+      .get() ( (err, response, body) => {
+
+      // err & response status checking code here
+      if (err != null || response.headers['content-type'].indexOf('application/json') === -1) {
+        robot.logger.error(err)
+        return res.send("An error occurred, or the answer was not JSON :(")
+      }
+
+      try {
+        const data = JSON.parse(body)
+        let message = "Something went wrong!"
+        if (data.success) {
+          message = `Service ${service} has version ${data.data.version} deployed`
+        }
+        return res.send(message)
+      } catch (error) {
+          robot.logger.error(error)
+          return res.send("Ran into an error parsing JSON :(")
+      }
+    })
+  })
+  
 }
